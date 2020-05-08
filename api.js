e78d1987b613f2b001238c154243eeaa79c3c4d9
yinchengnuo <yinnuo96@163.com>
2020-05-08 16:05:18 +0800

    1213

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const exec = require('child_process').exec;
+let browser;
+puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] }).then(e => browser = e)
 
 module.exports = router => {
     
@@ -10,7 +12,6 @@ module.exports = router => {
 
     router.get("/express", async (ctx) => { // 物流单号查询
         ctx.body = await new Promise(async resolve => {
-            const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
             const page = await browser.newPage()
             await page.setExtraHTTPHeaders({ 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36' })
             await page.goto('https://www.kuaidi100.com/', { timeout: 0 }) // 557006432812950
