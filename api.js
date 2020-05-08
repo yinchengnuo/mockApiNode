930d170e924c9234d45dc1750e7970eac8ae15e5
yinchengnuo <yinnuo96@163.com>
2020-05-08 21:35:48 +0800

    123

--- api.js
+++ api.js
@@ -13,17 +13,11 @@ module.exports = router => {
         ctx.body = await new Promise(async resolve => {
             const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors'] })
             const page = await browser.newPage()
-            await page.setExtraHTTPHeaders({ 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36' })
-            console.log('start:goto')
             await page.goto('https://www.kuaidi100.com/', { timeout: 0, waitUntil: 'networkidle2' }) // 557006432812950
-            console.log('end:goto')
             const input = await page.$('#postid')
             await input.type(ctx.request.query.num)
             const query = await page.$('#query')
             await query.click()
-            page.on('request', async req => {
-                // console.log(req._url)
-            })
             page.on('response', async res => {
                 if (res._url.includes('/query')) {
                     resolve(JSON.parse(await res.text()))
