ed6bef4f9ab60f34c7869b6d24cf5a35b333b5b4
yinchengnuo <yinnuo96@163.com>
2020-05-08 13:40:59 +0800

    2020

--- api.js
+++ api.js
@@ -12,7 +12,7 @@ module.exports = router => {
         console.log(ctx.request.query.num)
         if (ctx.request.query.num.length >= 9) {
             ctx.body = await new Promise(async resolve => {
-                const browser = await puppeteer.launch({ headless: false, args: ['–no-sandbox', '--disable-setuid-sandbox'] })
+                const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
                 const page = await browser.newPage()
                 page.goto(`https://www.baidu.com/s?wd=${ctx.request.query.num}`) // 557006432812950
                 page.on('response', async res => {
