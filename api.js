1d5722ac9d56288a6caa082b6818d41166147d1e
yinchengnuo <yinnuo96@163.com>
2020-05-08 19:42:11 +0800

    456

--- api.js
+++ api.js
@@ -11,11 +11,11 @@ module.exports = router => {
 
     router.get("/express", async (ctx) => { // 物流单号查询
         ctx.body = await new Promise(async resolve => {
-            const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors'] })
+            const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors'] })
             const page = await browser.newPage()
             await page.setExtraHTTPHeaders({ 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36' })
             console.log('start:goto')
-            await page.goto('https://www.kuaidi100.com/', { timeout: 0 }) // 557006432812950
+            await page.goto('https://www.kuaidi100.com/', { timeout: 0, waitUntil: 'networkidle2' }) // 557006432812950
             console.log('end:goto')
             const input = await page.$('#postid')
             await input.type(ctx.request.query.num)
