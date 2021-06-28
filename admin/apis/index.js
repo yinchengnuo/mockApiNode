2cf27099bc1fc2176e29e719a177936b7a324ae6
yinchengnuo <yinchengnuo@qq.com>
2021-06-28 12:09:45 +0800

    publish

--- admin/apis/index.js
+++ admin/apis/index.js
@@ -2,11 +2,12 @@ module.exports = router => {
 
     router.get('/', async ctx => {
         ctx.body = {
-            code: 100,
+            code: 200,
             message: 'Vue后台管理系统前端开发MOCK接口',
             data: {
                 by: '尹成诺',
-                tam: + new Date
+                tam: + new Date,
+                test: '周天德'
             }
         }
     })
